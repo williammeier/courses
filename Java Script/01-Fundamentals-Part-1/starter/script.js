53cdfe6e7cf8895b8ee3399389499ae44e3639d4
williammeier <william.meier@outlook.com>
2023-10-01 14:06:57 -0300

    upload courses

---
+++ Java Script/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firsName = "Jonas";
+console.log(firsName) */
+
+// javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'William');
+
+// javaScriptIsFun = 'YES!'
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
---
+++ Vue.js 3 -  by Matheus Battisti/2_project_vue/src/components/Info.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <p>CompEmail: {{ email }}</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
+    <p>Utilizo as seguintes tecnologias para back-end:</p>
+    <ul>
+      <li
+        v-for="(technology, index) in backend_technologies"
+        v-bind:key="index"
+      >
+        {{ technology }}
+      </li>
+    </ul>
+    <p>Utilizo as seguintes tecnologias para front-end:</p>
+    <ul>
+      <li v-for="technology in frontend_technologies" :key="technology.id">
+        {{ technology.language }}
+      </li>
+    </ul>
+    <div>
+      <button @click="showEmail">{{ textoButton }}</button>
+    </div>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>
+      Para acessar meu portfolio pasta
+      <a v-bind:href="meu_link" target="_blank">clicar aqui</a>
+    </p>
+    <Picture />
+  </div>
+</template>
+
+<script>
+import Picture from './Picture.vue'
+
+export default {
+  name: 'Info',
+  components: {
+    Picture,
+  },
+  props: {
+    email: String,
+    esta_trabalhando: Boolean,
+  },
+  data() {
+    return {
+      mostrar_email: false,
+      meu_link: 'https://williammeier.github.io/portfolio/',
+      textoButton: 'Mostrar email',
+      backend_technologies: ['Java Script', 'PHP', 'Python'],
+      frontend_technologies: [
+        { id: 1, language: 'HTML' },
+        { id: 2, language: 'CSS' },
+        { id: 3, language: 'VUE' },
+      ],
+    }
+  },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email
+      if (!this.mostrar_email) {
+        this.textoButton = 'Mostar email'
+      } else {
+        this.textoButton = 'Ocultar email'
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.paragrafo-pai {
+  color: red;
+}
+</style>
